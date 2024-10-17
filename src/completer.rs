eab371321a2d35b71c44cdc3f73042bb637a7375
Denis Redozubov <denis.redozubov@gmail.com>
2024-10-17 14:07:27 +0400

    Move completer

--- src/completer.rs
+++ src/completer.rs
@@ -1,22 +1,17 @@
+use crate::edgar::tickers;
 use rustyline::completion::{Completer, Pair};
 use rustyline::Context;
 use rustyline::Result;
-use crate::edgar::tickers::TICKERS;
 
 pub struct TickerCompleter;
 
 impl Completer for TickerCompleter {
     type Candidate = Pair;
 
-    fn complete(
-        &self,
-        line: &str,
-        pos: usize,
-        _ctx: &Context<'_>,
-    ) -> Result<(usize, Vec<Pair>)> {
+    fn complete(&self, line: &str, pos: usize, _ctx: &Context<'_>) -> Result<(usize, Vec<Pair>)> {
         if let Some(at_pos) = line[..pos].rfind('@') {
             let prefix = &line[at_pos + 1..pos].to_uppercase();
-            let candidates: Vec<Pair> = TICKERS
+            let candidates: Vec<Pair> = tickers::TICKER_DATA
                 .iter()
                 .filter(|(ticker, _)| ticker.starts_with(prefix))
                 .map(|(ticker, _)| Pair {
