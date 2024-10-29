3a2fd7d655630c5a14185eba65145708083d0b63
Denis Redozubov <denis.redozubov@gmail.com>
2024-10-29 13:07:34 +0400

    Update edgar::filing to include basic xbrl stripping/parsing

--- src/edgar/filing.rs
+++ src/edgar/filing.rs
@@ -1,12 +1,22 @@
 use anyhow::{anyhow, Result};
+use chardet::detect;
 use chrono::{DateTime, NaiveDate, NaiveDateTime};
+use encoding_rs::Encoding;
+use encoding_rs_io::DecodeReaderBytesBuilder;
+use html_escape::decode_html_entities;
 use log::{error, info, warn};
+use regex::Regex;
 use reqwest::Client;
+use scraper::{Html, Selector};
 use serde::{Deserialize, Serialize};
-use std::fs;
+use serde_json::json;
+use std::collections::HashMap;
+use std::fs::{self, File};
+use std::io::{BufReader, Read, Write};
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use url::Url;
+use uuencode::uudecode;
 
 use super::query::Query;
 use super::report::ReportType;
@@ -533,4 +543,164 @@ pub async fn fetch_matching_filings(client: &Client, query: &Query) -> Result<Ve
     futures::future::try_join_all(fetch_tasks).await?;
 
     Ok(matching_filings)
+}
+
+pub fn extract_complete_submission_filing(
+    filepath: &str,
+    output_directory: Option<&Path>,
+) -> Result<HashMap<String, serde_json::Value>> {
+    let elements_list = vec![
+        ("FILENAME", "<FILENAME>"),
+        ("TYPE", "<TYPE>"),
+        ("SEQUENCE", "<SEQUENCE>"),
+        ("DESCRIPTION", "<DESCRIPTION>"),
+    ];
+
+    let output_directory = output_directory.unwrap_or_else(|| Path::new(""));
+    if !output_directory.exists() {
+        fs::create_dir_all(output_directory)?;
+    }
+
+    info!("extracting documents to {:?}", output_directory);
+
+    let xbrl_doc = Regex::new(r"<DOCUMENT>(.*?)</DOCUMENT>")?;
+    let xbrl_text = Regex::new(r"<(TEXT|text)>(.*?)</(TEXT|text)>")?;
+
+    let raw_text = fs::read(filepath)?;
+    let charenc = detect(&raw_text).0;
+
+    let mut raw_text = DecodeReaderBytesBuilder::new()
+        .encoding(Encoding::for_label(charenc.as_bytes()))
+        .build(BufReader::new(File::open(filepath)?));
+
+    let mut raw_text_string = String::new();
+    raw_text.read_to_string(&mut raw_text_string)?;
+
+    let filing_header = header_parser(&raw_text_string)?;
+
+    let mut filing_documents = HashMap::new();
+    filing_documents.insert("header".to_string(), json!(filing_header));
+
+    let documents: Vec<_> = xbrl_doc
+        .find_iter(&raw_text_string)
+        .map(|m| m.as_str())
+        .collect();
+
+    for (i, document) in documents.iter().enumerate() {
+        let mut filing_document = HashMap::new();
+
+        // Extract document information
+        for (element, element_path) in &elements_list {
+            if let Some(value) = document
+                .split(element_path)
+                .nth(1)
+                .and_then(|s| s.split('<').next())
+            {
+                filing_document.insert(element.to_string(), value.trim().to_string());
+            }
+        }
+
+        let raw_text = xbrl_text
+            .captures(document)
+            .and_then(|cap| cap.get(2))
+            .map(|m| m.as_str())
+            .unwrap_or("")
+            .replace("<XBRL>", "")
+            .replace("</XBRL>", "")
+            .replace("<XML>", "")
+            .replace("</XML>", "")
+            .trim()
+            .to_string();
+
+        let doc_num = format!("{:04}", i + 1);
+        let output_filename = format!(
+            "{}-({}) {} {}",
+            doc_num,
+            filing_document.get("TYPE").unwrap_or(&"".to_string()),
+            filing_document
+                .get("DESCRIPTION")
+                .unwrap_or(&"".to_string()),
+            filing_document.get("FILENAME").unwrap_or(&"".to_string())
+        );
+        let output_filename = &output_filename;
+        let output_filepath = output_directory.join(&output_filename);
+
+        if raw_text.to_lowercase().starts_with("begin")
+            || document.to_lowercase().starts_with("begin")
+        {
+            // Handle UUEncoded content
+            let decoded_content = uudecode(&raw_text)
+                .ok_or_else(|| anyhow::anyhow!("Failed to decode UUEncoded content"))?;
+            let mut file = File::create(&output_filepath)?;
+            file.write_all(&decoded_content.0)?; // Write the Vec<u8> part
+        } else {
+            fs::write(&output_filepath, raw_text)?;
+        }
+
+        filing_document.insert(
+            "RELATIVE_FILEPATH".to_string(),
+            output_filepath.to_str().unwrap().to_string(),
+        );
+        filing_document.insert("DESCRIPTIVE_FILEPATH".to_string(), output_filename.clone());
+        filing_document.insert("FILE_SIZE".to_string(), file_size(&output_filepath)?);
+        filing_document.insert(
+            "FILE_SIZE_BYTES".to_string(),
+            fs::metadata(&output_filepath)?.len().to_string(),
+        );
+
+        filing_documents.insert(i.to_string(), json!(filing_document));
+    }
+
+    Ok(filing_documents)
+}
+
+pub fn header_parser(raw_html: &str) -> Result<Vec<(String, String)>> {
+    let document = Html::parse_document(raw_html);
+    let sec_header_selector = Selector::parse("sec-header").unwrap();
+
+    let mut data = Vec::new();
+
+    if let Some(sec_header_element) = document.select(&sec_header_selector).next() {
+        let sec_header_html = sec_header_element.inner_html();
+        let re = Regex::new(r"<(SEC-HEADER|sec-header)>(.*?)</(SEC-HEADER|sec-header)>")?;
+
+        if let Some(captures) = re.captures(&sec_header_html) {
+            if let Some(sec_header) = captures.get(2) {
+                let split_header: Vec<&str> = sec_header.as_str().split('\n').collect();
+
+                let mut current_group = String::new();
+                for header_item in split_header.iter() {
+                    let header_item = header_item.trim();
+                    if !header_item.is_empty() {
+                        if header_item.starts_with('<') && header_item.ends_with('>') {
+                            current_group = header_item.to_string();
+                        } else if !header_item.starts_with('\t') && !header_item.contains('<') {
+                            if let Some(colon_index) = header_item.find(':') {
+                                let key = header_item[..colon_index].trim();
+                                let value =
+                                    decode_html_entities(&header_item[colon_index + 1..].trim())
+                                        .into_owned();
+                                data.push((format!("{}:{}", current_group, key), value));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(data)
+}
+
+fn format_filename(filename: &str) -> String {
+    filename
+        .replace(" ", "_")
+        .replace(":", "")
+        .replace("__", "_")
+}
+
+fn file_size(filepath: &Path) -> Result<String> {
+    let metadata = fs::metadata(filepath)?;
+    let size = metadata.len();
+    Ok(format!("{:.2} KB", size as f64 / 1024.0))
 }
