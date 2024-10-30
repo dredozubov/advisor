98111bacaa651afd960167e27715e4cd6b7f9476
Denis Redozubov <denis.redozubov@gmail.com>
2024-10-30 16:02:25 +0400

    refactor: Simplify re-exports in XBRL module

--- src/edgar/parsing/xbrl/mod.rs
+++ src/edgar/parsing/xbrl/mod.rs
@@ -1,11 +1,7 @@
 pub mod parser;
 
 // Re-export types from xml parser
-pub use self::parser::xml::{XBRLFiling, FactItem, FactTableRow, DimensionTableRow};
-pub use super::types::{FilingDocument, FilingFact, Period};
+pub use self::parser::xml::{DimensionTableRow, FactItem, FactTableRow, XBRLFiling};
 
-// Re-export for public API
-pub use self::parser::xml::XBRLFiling;
-pub use self::parser::xml::FactItem;
-pub use self::parser::xml::FactTableRow;
-pub use self::parser::xml::DimensionTableRow;
+// Re-export core functionality
+pub use super::types::{FilingDocument, FilingFact, Period};
