298397e461e67d3be5d640f58a622988bffd4647
Denis Redozubov <denis.redozubov@gmail.com>
2024-11-09 18:02:52 +0400

    feat: Add Qdrant and vector storage modules to storage implementation

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,9 +1,3 @@
 pub mod memory;
-use langchain_rust::vectorstore::VectorStore;
-pub use memory::InMemoryStore;
-
-#[async_trait::async_trait]
-pub trait VectorStorage: VectorStore {
-    fn is_ephemeral(&self) -> bool;
-    // Other methods from VectorStore...
-}
+pub mod qdrant;
+pub mod vector_storage;
