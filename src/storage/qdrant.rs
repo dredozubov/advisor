05b22605134187aedf348bd31006e2b094d52b11
Denis Redozubov <denis.redozubov@gmail.com>
2024-11-09 17:54:55 +0400

    refactor: Simplify QdrantStorage implementation and remove unimplemented methods

--- src/storage/qdrant.rs
+++ src/storage/qdrant.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use anyhow::Result;
 use async_trait::async_trait;
-use langchain_rust::{embedding::Embedder, schemas::Document};
+use langchain_rust::{embedding::Embedder, schemas::Document, vectorstore::VectorStore};
 use qdrant_client::{config::QdrantConfig, Qdrant};
 
 use super::vector_storage::VectorStorage;
@@ -13,34 +13,14 @@ pub struct QdrantStoreConfig {
     pub collection_name: String,
 }
 
-pub struct QdrantStorage: VectorStore {
+pub struct QdrantStorage {
     client: Qdrant,
     embedder: Arc<dyn Embedder>,
 }
 
 #[async_trait]
 impl VectorStore for QdrantStorage {
-    fn is_ephemeral(&self) -> bool {
-        false
-    }
-
-    type Config = QdrantStoreConfig;
-
-    async fn new(config: Self::Config, embedder: EmbedderWrapper) -> Result<Self> {
-        let qdrant_config = QdrantConfig::from_url(&config.uri);
-        let client = Qdrant::new(qdrant_config)
-            .map_err(|e| anyhow::anyhow!("Failed to create Qdrant client: {}", e))?;
-
-        Ok(Self { client, embedder })
-    }
-
-    fn is_in_memory(&self) -> bool {
-        false
-    }
-
     async fn add_documents(&self, _documents: Vec<(Document, DocumentMetadata)>) -> Result<()> {
-        // TODO: Implement document addition for Qdrant
-        log::warn!("Document addition not yet implemented for Qdrant storage");
         Ok(())
     }
 
